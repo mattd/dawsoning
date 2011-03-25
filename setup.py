95592b395cb7cd4a1d511807b5ab57de1868bb6e
Matt Dawson <matthewtdawson@gmail.com>
2011-03-25 17:54:12 -0400

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Just some family's website."""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Dawsoning',
+    version='2.0',
+    url='http://dawsoning.com/',
+    author='Matt Dawson',
+    author_email='matt@dawsoning.com',
+    description="Just some family's website.",
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'Django',
+        'jellyroll',
+        'feedparser',
+    ],
+    tests_require='nose',
+    test_suite='nose.collector',
+)
